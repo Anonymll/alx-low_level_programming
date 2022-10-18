7dab5c041ce8707f23d3cc42db521aa1f339bd5b
Anonymll <108156995+Anonymll@users.noreply.github.com>
2022-10-18 23:12:46 +0100

    new commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,8 +6,18 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
+
+int main()  
+{  
+char ch = 'a';
+  
+printf(« Alphabets anglais minuscules:\n »);
+    while(ch <= 'z')  
+    {  
+printf(« %c « , ch);
+        ch++;  
+    }  
+printf(« \n »);
+  
+return 0;
+}  
